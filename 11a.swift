284e30b422f3e4eb58673e741b5c2d59b2e6c28a
Jindrich Stepanek <stepanek.jin@gmail.com.com>
2021-12-11 15:03:28 +0100

    11

---
+++ 11a.swift
@@ -0,0 +1,23 @@
+func adjacentIndexes(_ index: (Int, Int)) -> [(Int, Int)] {
+	(-1...1).flatMap { r in (-1...1).map { c in (index.0+r, index.1+c) } }
+		.filter { $0 != index && (0...9) ~= $0.0 && (0...9) ~= $0.1 }
+}
+
+var octopuses: [[Int]] = (1...10).map { _ in readLine()!.map(String.init).compactMap(Int.init) }
+let allIndexes: [(Int, Int)] = (0...9).flatMap { r in (0...9).map { c in (r, c) } }
+
+var flashed = 0
+for _ in (1...10) {
+	allIndexes.forEach { octopuses[$0][$1] = octopuses[$0][$1] + 1 }
+	var queue = allIndexes.filter { octopuses[$0][$1] > 9 }
+	while queue.count > 0 {
+		let index = queue.removeFirst()
+		if octopuses[index.0][index.1] == 0 { continue }
+		octopuses[index.0][index.1] = 0
+		let adjacent = adjacentIndexes(index).filter { octopuses[$0][$1] > 0 }
+		adjacent.forEach { octopuses[$0][$1] = octopuses[$0][$1] + 1 }
+		queue += adjacent.filter { octopuses[$0][$1] > 9 }
+		flashed += 1
+	}
+}
+print(flashed)
